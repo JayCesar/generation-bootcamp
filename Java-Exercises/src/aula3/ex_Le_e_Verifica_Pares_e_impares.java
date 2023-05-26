27fab2b086607964efa5a3edc0eb3ba86d2ddc9a
JayCesar <batistajulio215@gmail.com>
2023-05-26 14:02:50 -0300

    update: add new exercise

---
+++ Java-Exercises/src/aula3/ex_Le_e_Verifica_Pares_e_impares.java
@@ -0,0 +1,29 @@
+package aula3;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex_Le_e_Verifica_Pares_e_impares {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		int totalEven = 0;
+		int totalOdd = 0;
+	
+		for (int i = 1; i < 11; i++) {
+			System.out.print("Digite o " + i + "º número: ");
+			int inputNumber = sc.nextInt();
+			totalEven += (inputNumber % 2 == 0) ? 1 : 0;
+			totalOdd += (inputNumber % 2 != 0) ? 1 : 0;
+		}
+		
+		System.out.println("\nTotal de números pares: " + totalEven);
+		System.out.println("\nTotal de números ímpares: " + totalOdd);
+		
+		
+		sc.close();
+
+	}
+
+}
