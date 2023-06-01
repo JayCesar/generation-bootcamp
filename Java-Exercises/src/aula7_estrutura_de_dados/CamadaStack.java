22ff2e08252717cc395bd03cf51e8c1b7febf8a2
JayCesar <batistajulio215@gmail.com>
2023-06-01 15:01:33 -0300

    update: add pacakges about methods

--- Java-Exercises/src/aula7_estrutura_de_dados/CamadaStack.java
+++ Java-Exercises/src/aula7_estrutura_de_dados/CamadaStack.java
@@ -73,7 +73,7 @@ public class CamadaStack {
 			System.out.println("\nPrograma Finalizado!");
 			return 0;
 		} else {
-			clearScreen();
+			// clearScreen();
 			printMenu();
 			System.out.print("\nEntre com a opção desejada: ");
 			int choiceList = sc.nextInt();
@@ -117,10 +117,6 @@ public class CamadaStack {
 		}
 	}
 	
-	// Clear screen - Funcioan apenas no terminal do git
-	public static void clearScreen() {  
-	    System.out.print("\033[H\033[2J");  
-	    System.out.flush();  
-	}  
+	
 
 }
